d14d8421b1fb88dd7d0484f7ca89f4e0e6bd4c66
FernandaMontanaro <fermontanaro@gmail.com>
2023-09-10 22:25:15 -0600

    comiy

---
+++ src/componentes/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './home.css'
+import Presentacion from '../presentacion/Presentacion'
+import Conocenos from '../conocenos/Conocenos'
+
+function Home() {
+  return (
+    <>
+    <div className='contenedor-home'>
+    <Presentacion />
+    </div>
+    </>
+    
+  )
+}
+
+export default Home
